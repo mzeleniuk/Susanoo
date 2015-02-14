90211d7a2d84cf28814bb7bace9307e78552d11e
Michael Zelenyuk <zell.xp@gmail.com>
2015-02-14 21:54:11 +0200

    Create Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,8 +8,23 @@ class Product < ActiveRecord::Base
                       }
   validates :title, length: {minimum: 10}
 
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   def self.latest
     Product.order(:updated_at).last
+  end
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'existing line items')
+      return false
+    end
   end
 
 end
