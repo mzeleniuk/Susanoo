e2a41e76c2e9ae86479f862e10817292dbd27c4d
Michael Zelenyuk <zell.xp@gmail.com>
2015-02-20 21:42:15 +0200

    Add order notifier

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,25 @@
+class OrderNotifier < ApplicationMailer
+  default from: 'Susanoo <susanoo@example.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: 'Susanoo Order Confirmation'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: 'Susanoo Order Shipped'
+  end
+end
---
+++ test/mailers/order_notifier_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class OrderNotifierTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Susanoo Order Confirmation", mail.subject
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal ["susanoo@example.com"], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
+  end
+
+  test "shipped" do
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Susanoo Order Shipped", mail.subject
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal ["susanoo@example.com"], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
+  end
+end
