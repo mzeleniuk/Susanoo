45fc22dc6a848ae2c778938d73db036d1e90acca
Michael Zelenyuk <zell.xp@gmail.com>
2015-02-21 17:33:28 +0200

    Add admitistration system

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  skip_before_action :authorize, only: [:create, :update, :destroy]
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
@@ -79,5 +80,4 @@ class CartsController < ApplicationController
     def cart_params
       params[:cart]
     end
-
 end
